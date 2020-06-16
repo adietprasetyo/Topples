88806d13ce326a035b31f039e126c23393cc6d3f
adietprasetyo <adietprasetyo99@gmail.com>
2020-06-16 18:12:14 +0700

    Tambah

---
+++ app/Http/Controllers/DiskusiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Diskusi;
+use Illuminate\Http\Request;
+
+class DiskusiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Diskusi  $diskusi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Diskusi $diskusi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Diskusi  $diskusi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Diskusi $diskusi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Diskusi  $diskusi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Diskusi $diskusi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Diskusi  $diskusi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Diskusi $diskusi)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/InfoProdukController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InfoProduk;
+use Illuminate\Http\Request;
+
+class InfoProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InfoProduk  $infoProduk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InfoProduk $infoProduk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InfoProduk  $infoProduk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InfoProduk $infoProduk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InfoProduk  $infoProduk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InfoProduk $infoProduk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InfoProduk  $infoProduk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InfoProduk $infoProduk)
+    {
+        //
+    }
+}
